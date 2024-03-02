5508cda5bedc0450cda31c15ef0342a49b9e5326
itumbi <imanase37@gmail.com>
2024-03-02 14:28:58 -0800

    first commit

---
+++ Untitled.py
@@ -0,0 +1,19 @@
+a,b,c=5,3.2,"Hello"
+print(a,b,c)
+num1=5
+print(num1, "is of type",  type(num1))
+list=[10, 20, 30]
+list.append(40)
+print(list)
+list[0]=50
+print(list)
+tuple=(10, 20, 30)
+#tuple.remove(10)
+#print(tuple)
+
+print("good morning", end=" ")
+print("it is rainy")
+# input
+num=input( "inter a number:")
+print("You entered a number", num)
+print("Data type", type(num)) 
